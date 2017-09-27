9a3da6cf8e9bfd4464804149dd5e41c77b57ea07
Jon <wildjona@msu.edu>
2017-09-26 22:50:11 -0400

    Moving MVC project to Github
    Moving Files

---
+++ CalcApp/WebApplication1/Controllers/CalcController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+        /*
+        public string Index()
+        {
+            //return View();
+            return "This is my <b>default</b> action...";
+        }
+        */
+
+        ////
+        // 
+        // GET: /Calc/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method.";
+        }
+        ////
+
+
+    }
+}
